04a84f7142811ea5f5fe557becd272c9f460cea7
JacobWoodbury <you@example.comjacobswoodbury@gmail.comgit config --global user.email jacobswoodbury@gmail.com>
2025-09-24 13:09:45 -0700

    challenge 2

---
+++ index.jsx
@@ -0,0 +1,7 @@
+import {createRoot} from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+root.render(<ul>
+    <li>Faster Development</li>
+    <li>Reactive Pages</li>
+    <li>Easy App Creation</li>
+</ul>)
